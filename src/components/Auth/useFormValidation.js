e54b4ea2aeff4766c1babb70df4b5e8dd0afc629
Matt Jackson <matt1383@churchofjesuschrist.org>
2020-01-29 17:21:00 -0700

    Initial commit

---
+++ src/components/Auth/useFormValidation.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+function useFormValidation(initialstate, validate, authenticate) {
+  const [values, setValues] = React.useState(initialstate);
+  const [errors, setErrors] = React.useState({});
+  const [isSubmitting, setSubmitting] = React.useState(false);
+
+  React.useEffect(() => {
+    if (isSubmitting) {
+      const noErros = Object.keys(errors).length === 0;
+      if (noErros) {
+        authenticate();
+        setSubmitting(false);
+      } else {
+        setSubmitting(false);
+      }
+    }
+  }, [errors]);
+
+  function handleChange(event) {
+    event.persist();
+    setValues(prevState => ({
+      ...prevState,
+      [event.target.name]: event.target.value
+    }));
+  }
+
+  function handleBlur() {
+    const validationErrors = validate(values);
+    setErrors(validationErrors);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const validationErrors = validate(values);
+    setErrors(validationErrors);
+    setSubmitting(true);
+  }
+
+  return {
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    values,
+    errors,
+    isSubmitting
+  };
+}
+
+export default useFormValidation;
